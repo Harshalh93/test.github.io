e70600b58c036d3c8b1e0aa5ac6711f14c6092b3
Harshalh93 <71639619+Harshalh93@users.noreply.github.com>
2020-09-21 17:13:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function love() {
  heart.innerHTML = "♥";
  var msg = document.createElement("img");
  msg.src =
-  "https://media.tenor.com/images/a3edb9b14b72a74373debf57a2e448ec/tenor.gif";
+  "/root/Animated_Heart_GIF_Picture.gif";
  heart.appendChild(brk);
  heart.appendChild(msg);
 }
